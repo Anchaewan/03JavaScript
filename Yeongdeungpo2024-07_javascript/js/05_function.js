484263f9b3096f03c0d56b63139189c99c8fa0b1
Anchaewan <dkscodhks@gmail.com>
2024-04-30 17:46:37 +0900

    4/30 완성

--- Yeongdeungpo2024-07_javascript/js/05_function.js
+++ Yeongdeungpo2024-07_javascript/js/05_function.js
@@ -2,7 +2,7 @@
 function name() {
   console.log("함수실행");
 }
-name();
+// name();
 
 // 익명함수 ( 이름이 없는 함수 )
 const getName = function (name) {
@@ -16,13 +16,13 @@ const getName = function (name) {
 // 반환
 
 // 매개변수
-function sum(num1, num2) {
-  return num1 + num2;
-  // console.log(num1 + num2);
-}
+// function sum(num1, num2) {
+//   return num1 + num2;
+// console.log(num1 + num2);
+// }
 //NaN : Not a Number
-const a = sum(2, "3");
-const b = sum(10, 20);
+// const a = sum(2, "3");
+// const b = sum(10, 20);
 // console.log(a);
 // console.log(b);
 
@@ -30,7 +30,35 @@ const b = sum(10, 20);
 function sayHi() {
   console.log("안녕하세요?");
 }
-sayHi();
+// sayHi();
 
 // 화살표 함수
+const sumResult = (a, b) => {
+  // 함수식
+  return a + b;
+};
+
+// console.log(sumResult(5, 6));
+
 // 호출 스케줄링
+
+const startButton = document.querySelector(".start");
+
+startButton.addEventListener("click", () => {
+  // setTimeout(() => {
+  // 실행식
+  // console.log("확인해주세요.");
+  // }, 2000);
+  //   setInterval(() => {
+  //     console.log("2초애 한번씩 실행");
+  //   }, 2000);
+});
+
+const stopButton = document.querySelector(".stop");
+setInterval(() => {
+  console.log("반복 텍스트");
+}, 2000);
+
+stopButton.addEventListener("click", () => {
+  clearInterval(interval);
+});
--- Yeongdeungpo2024-07_javascript/js/07_dom.js
+++ Yeongdeungpo2024-07_javascript/js/07_dom.js
@@ -1,4 +1,6 @@
 // 선택자
+const block = document.querySelector(".block1");
+console.log(typeof block);
 // 클래스제어
 // Text제어
 // 속성제어
